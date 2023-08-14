529078e10a20279ff23f066d93a01e27decc2b05
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-08-14 15:40:01 +0300

    + 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryUL: document.querySelector('.gallery'),
+};
+
+const markup = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}"
+      alt="${description}" />
+   </a>
+   </li>`;
+  })
+  .join('');
+
+refs.galleryUL.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  loop: true,
+});
