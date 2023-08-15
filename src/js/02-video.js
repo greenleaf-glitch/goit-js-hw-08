237935ed535ba723cbcc686cc1389c1c4ea9ee19
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-08-15 12:18:10 +0300

    fin v 1.0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,32 @@
-// const player = new Vimeo.Player('made-in-ny', options);
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const pageLeave = document.querySelector('a');
+
+const player = new Vimeo(iframe);
+
+player.setVolume(0.6);
+
+let currentPlayTime;
+
+const onPlay = function (data) {
+  currentPlayTime = JSON.stringify(data.seconds);
+  localStorage.setItem('videoplayer-current-time', currentPlayTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+try {
+  const currentPlayTimeReceived = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+  player.setCurrentTime(currentPlayTimeReceived);
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+}
+
+pageLeave.addEventListener('click', () => {
+  localStorage.removeItem('videoplayer-current-time');
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,63 @@
+import throttle from 'lodash.throttle';
 
+refs = {
+  form: document.querySelector('form'),
+};
+
+let formContent = {};
+const storageKey = 'feedback-form-state';
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onSubmit);
+
+updateFormOutput();
+
+function onFormInput(evt) {
+  evt.preventDefault();
+
+  const value = evt.target.value;
+  const key = evt.target.name;
+  formContent[key] = value;
+
+  storageSave(storageKey, formContent);
+}
+
+function updateFormOutput() {
+  const dataFromStorage = storageLoad(storageKey) || {};
+
+  refs.form.elements.email.value = dataFromStorage?.email || '';
+  refs.form.elements.message.value = dataFromStorage?.message || '';
+
+  formContent = dataFromStorage;
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  storageRemove(storageKey);
+  formContent = {};
+}
+
+// ======== Ф-ії роботи зi сховищем можна винести в окремий файл, але умовами задачі не передбачено =======
+
+function storageSave(key, value) {
+  try {
+    const str = JSON.stringify(value);
+    localStorage.setItem(key, str);
+  } catch (error) {
+    console.error('Set error: ', error.message);
+  }
+}
+
+function storageLoad(key) {
+  try {
+    const str = localStorage.getItem(key);
+    return str === null ? undefined : JSON.parse(str);
+  } catch (error) {
+    console.error('Get error: ', error.message);
+  }
+}
+
+function storageRemove(key) {
+  localStorage.removeItem(key);
+}
