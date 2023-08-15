39a6909273dab1d054d34e8401ce04751a4f6d28
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-08-15 14:46:00 +0300

    + storage.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,5 @@
 import throttle from 'lodash.throttle';
+import { storageSave, storageLoad, storageRemove } from 'storage.js';
 
 const refs = {
   form: document.querySelector('form'),
@@ -37,28 +38,4 @@ function onSubmit(evt) {
   storageRemove(storageKey);
   console.log(formContent);
   formContent = {};
-}
-
-// ======== Ф-ії роботи зi сховищем можна винести в окремий файл, але умовами задачі не передбачено =======
-
-function storageSave(key, value) {
-  try {
-    const str = JSON.stringify(value);
-    localStorage.setItem(key, str);
-  } catch (error) {
-    console.error('Set error: ', error.message);
-  }
-}
-
-function storageLoad(key) {
-  try {
-    const str = localStorage.getItem(key);
-    return str === null ? undefined : JSON.parse(str);
-  } catch (error) {
-    console.error('Get error: ', error.message);
-  }
-}
-
-function storageRemove(key) {
-  localStorage.removeItem(key);
 }
---
+++ src/js/storage.js
@@ -0,0 +1,27 @@
+function storageSave(key, value) {
+  try {
+    const str = JSON.stringify(value);
+    localStorage.setItem(key, str);
+  } catch (error) {
+    console.error('Set error: ', error.message);
+  }
+}
+
+function storageLoad(key) {
+  try {
+    const str = localStorage.getItem(key);
+    return str === null ? undefined : JSON.parse(str);
+  } catch (error) {
+    console.error('Get error: ', error.message);
+  }
+}
+
+function storageRemove(key) {
+  localStorage.removeItem(key);
+}
+
+export default {
+  storageSave,
+  storageLoad,
+  storageRemove,
+};
